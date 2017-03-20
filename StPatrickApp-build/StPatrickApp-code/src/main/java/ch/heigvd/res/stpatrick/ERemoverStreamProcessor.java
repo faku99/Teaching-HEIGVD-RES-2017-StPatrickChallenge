d3ce509b95c30c58935343881f2fd475e4a9b148
Lucas Elisei <faku99dev@gmail.com>
2017-03-20 11:46:33 +0100

    Second test passes

---
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/ERemoverStreamProcessor.java
@@ -0,0 +1,23 @@
+package ch.heigvd.res.stpatrick;
+
+import java.io.*;
+
+public class ERemoverStreamProcessor implements IStreamProcessor {
+    private int forbiddenLowerCase = (int)'e';
+    private int forbiddenUpperCase = (int)'E';
+
+    @Override
+    public void process(Reader in, Writer out) throws IOException {
+        BufferedReader br = new BufferedReader(in);
+        BufferedWriter bw = new BufferedWriter(out);
+        int c = br.read();
+        while (c != -1) {
+            if(c != forbiddenLowerCase && c != forbiddenUpperCase) {
+                bw.write(c);
+            }
+
+            c = br.read();
+        }
+        bw.flush();
+    }
+}
